2295f98d00f8531aebee98dfe5659f1d23d926ed
namekinggamerboy <53903319+namekinggamerboy@users.noreply.github.com>
2020-05-26 18:34:52 +0530

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,4 +1,3 @@
-if (process.version.slice(1).split('.')[0] < 12) return console.log('Node 12.0.0 or higher is required.');
 const { ShardingManager } = require('discord.js');
 
 module.exports = {
@@ -6,6 +5,7 @@ module.exports = {
   version: require("./package.json").version,
 
   start(op){
+if (process.version.slice(1).split('.')[0] < 12) return console.log('Node 12.0.0 or higher is required.');
 
 const manager = new ShardingManager(op.filename, {
     totalShards: op.totalshard||"auto",   
@@ -17,4 +17,5 @@ manager.spawn();
 manager.on('shardCreate', (shard) => console.log(`🎉 | Shard ${shard.id} launched`));
 
   }
+
 }
